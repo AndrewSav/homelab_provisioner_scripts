ef5f52591885396a50c560ff67f98a8fc515f05c
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2020-10-10 11:32:27 +1300

    disable root login over ssh

--- disable_provisioning_user.sh
+++ disable_provisioning_user.sh
@@ -2,6 +2,11 @@
 
 set -e
 USR=$1
+
 sudo sed -E -i 's|^#?(PasswordAuthentication)\s.*|\1 no|' /etc/ssh/sshd_config
 if [[ ! $(sudo grep '^PasswordAuthentication\s' /etc/ssh/sshd_config) ]]; then echo 'PasswordAuthentication no' |sudo tee -a /etc/ssh/sshd_config; fi
+
+sudo sed -E -i 's|^#?(PermitRootLogin)\s.*|\1 no|' /etc/ssh/sshd_config
+if ! grep '^PermitRootLogin\s' /etc/ssh/sshd_config; then echo 'PermitRootLogin no' |sudo tee -a /etc/ssh/sshd_config; fi
+
 sudo passwd "$USR" -l
